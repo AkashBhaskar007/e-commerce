bee8f2f08ceb3d15a371190f2bba0004ebe569dd
Akash Bhaskar <i.am.akashbhaskar@accubits.com>
2021-12-15 21:01:01 +0530

    changes

--- modules/productModules/cartController.js
+++ modules/productModules/cartController.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const { createCart, deleteCartService } = require('./productService')
 const Cart = require('../../models/cart')
+
+
+
 exports.cartController = async (req, res) => {
     let { id } = req.params;
     const userID = req.decoded.id;
@@ -13,6 +16,8 @@ exports.cartController = async (req, res) => {
         .populate('userID productID', 'productName productDescription');
     return res.send({ message: 'Product added to cart!', cart });
 }
+
+
 exports.deleteCartController = async (req, res) => {
     let { id } = req.params;
     const cart = await deleteCartService(id);
